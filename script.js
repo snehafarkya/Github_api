bb45c149f93dcf2ac9dd8d497c04e2e31aaba51f
snehafarkya <snhafarkya@gmail.com>
2023-01-03 12:05:33 +0530

    More info added

--- script.js
+++ script.js
@@ -9,17 +9,16 @@
 //   console.log("Valid")
 // }
 let names = prompt("Enter github username")
-fetch(`https://api.github.com/users/${names}`, { 
-                 headers: {
-                      'Accept' : 'application/vnd.github.v3+json'
-                  }})
+fetch(`https://api.github.com/users/${names}`)
 		.then(response => response.json()) //Converting the response to a JSON object
 		.then( data => {
                     const root = document.querySelector('#root');
                     root.innerHTML = `<a href='${data.html_url}'>Name: '${data.name}'</a>
                      <p>Followers : '${data.followers}'</p>
+                     <p>Following : '${data.following}'</p>
                      <p>Location : '${data.location}'</p>
                      <p>Twitter username : '${data.twitter_username}'</p>
-                     <p>Created at : '${data.created_at}'</p>`
+                     <p>Created at : '${data.created_at}'</p>
+                     <p>Your Bio : '${data.bio}'</p>`
                 })
 		.catch( error => console.error(error));
