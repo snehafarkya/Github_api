69bba3dc6dad43a09ef4df193f8372a7e09a6e1d
snehafarkya <snhafarkya@gmail.com>
2023-01-03 12:57:35 +0530

    Css added

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ fetch(`https://api.github.com/users/${names}`)
 		.then(response => response.json()) //Converting the response to a JSON object
 		.then( data => {
                     const root = document.querySelector('#root');
-                    root.innerHTML = `<a href='${data.html_url}'>Name: '${data.name}'</a>
+                    root.innerHTML = `<a class='name' href='${data.html_url}'>Name: '${data.name}'</a>
                      <p>Followers : '${data.followers}'</p>
                      <p>Following : '${data.following}'</p>
                      <p>Location : '${data.location}'</p>
                      <p>Twitter username : '${data.twitter_username}'</p>
                      <p>Created at : '${data.created_at}'</p>
-                     <p>Your Bio : '${data.bio}'</p>`
+                     <p>Your Bio : '${data.bio}'</p>
+                     <p>Your contributions : '${data.repos_url}'</p>`
+                     
                 })
 		.catch( error => console.error(error));
